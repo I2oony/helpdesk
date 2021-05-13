ebf725270cf33c0efa0e70aa685c8bae274283fa
I2oony <ppalamar28@gmail.com>
2021-05-13 18:46:32 +0300

    frontend: added generated top bar and menu plug

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,3 +1,5 @@
+checkAuth();
+
 let body = document.querySelector('body');
 let authForm = document.getElementById('login-form');
 
@@ -8,24 +10,36 @@ authForm.onsubmit = function() {
     var password = document.getElementById('login-field-password').value;
 
     axios.get(url, {
-      withCredentials: true,
-      auth: {
-        username: username,
-        password: password
-      }
-    })
+        withCredentials: true,
+        auth: {
+            username: username,
+            password: password
+        }
+        })
     .then(function (response) {
-      var cookie = response.data;
-      document.cookie = "token=" + cookie + "; secure";
-      window.location.pathname = "/web/dashboard"
+        var cookie = response.data;
+        document.cookie = "token=" + cookie + "; secure";
+        window.location.pathname = "/web/dashboard"
     })
     .catch(function (error) {
-      if (document.getElementById("id-error-message") == null) {
-        let span = document.createElement('span');
-        span.className = "error-message font-subtitle-1";
-        span.id = "id-error-message";
-        span.innerHTML = "Incorrect login or password!";
-        authForm.prepend(span);
-      }
+        if (document.getElementById("id-error-message") == null) {
+            let span = document.createElement('span');
+            span.className = "error-message font-subtitle-1";
+            span.id = "id-error-message";
+            span.innerHTML = "Incorrect login or password!";
+            authForm.prepend(span);
+        }
     })
 };
+
+function checkAuth() {
+    let url = "https://i2oony.com/api/users";
+
+    axios.get(url)
+        .then(function (response) {
+            window.location.pathname = "/web/dashboard"
+        })
+        .catch(function (error) {
+            console.log()
+        })
+}
---
+++ frontend/web/dashboard/checkAuth.js
@@ -0,0 +1,20 @@
+let url = "https://i2oony.com/api/users";
+var user;
+
+const getUser = async () => {
+    await axios.get(url)
+        .then(function (response) {
+            user = response.data;
+
+            let body = document.querySelector('body');
+            let script = document.createElement('script');
+            script.src = "interface.js";
+            body.appendChild(script);
+        })
+        .catch(function (error) {
+            console.log(error)
+            window.location.pathname = "/";
+        })
+}
+
+getUser();
