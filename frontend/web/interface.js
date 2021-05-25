3bd0ae8d6c406bb3814a7339c76cd1a9380f6ff2
I2oony <ppalamar28@gmail.com>
2021-05-25 04:24:52 +0300

    front: added users list on system page

--- frontend/web/interface.js
+++ frontend/web/interface.js
@@ -18,6 +18,7 @@ function buildPage(user) {
             break;
         case "/web/system/":
             title = "Настройки системы";
+            buildSystemPage();
             break;
         default:
             title = "Ошибка!"
@@ -132,13 +133,7 @@ function buildProfilePage(user) {
     changePasswordBlock.append(buildInputField("password", "profile-field-old-password", "Старый пароль"));
     changePasswordBlock.append(buildInputField("password", "profile-field-new-password", "Новый пароль"));
     changePasswordBlock.append(buildInputField("password", "profile-field-repeat-password", "Повтор пароля"));
-
-    let button = document.createElement('button');
-    button.className = "button-text z-axis-1 font-button";
-    button.textContent = "Сменить";
-    button.onclick = changePass;
-
-    changePasswordBlock.append(button);
+    changePasswordBlock.append(buildButton("Сменить", changePass));
 
     profilePage.append(infoBlock);
     profilePage.append(changePasswordBlock);
@@ -220,6 +215,128 @@ function changePass() {
     }
 }
 
+async function buildSystemPage() {
+    let systemPage = buildMainContent("system-page");
+
+    let createUserBlock = buildBlock("create-user-block");
+    createUserBlock.append(buildBlockHeader("Создать пользователя"));
+    createUserBlock.append(buildInputField("text", "username", "Логин"));
+    createUserBlock.append(buildInputField("text", "first-name", "Имя"));
+    createUserBlock.append(buildInputField("text", "last-name", "Фамилия"));
+    createUserBlock.append(buildInputField("text", "email", "Адрес электронной почты"));
+    let roles = [
+        {value: "client", text: "Клиент"},
+        {value: "operator", text: "Оператор"},
+        {value: "admin", text: "Администратор"}];
+    createUserBlock.append(buildDropdown(roles));
+    createUserBlock.append(buildButton("Создать", createUser));
+
+    let usersListBlock = buildBlock("users-list-block");
+    usersListBlock.append(buildBlockHeader("Список пользователей"));
+    let usersList = await fetchUsersList();
+    let usersTable = document.createElement('table');
+    usersTable.id = "users-table";
+    var header = {
+        username: "Логин",
+        firstName: "Имя",
+        lastName: "Фамилия",
+        email: "Электронная почта",
+        role: "Роль"}
+    usersTable.append(buildUserRow(-1, header));
+    for (user in usersList) {
+        usersTable.append(buildUserRow(user, usersList[user]));
+    }
+    usersListBlock.append(usersTable);
+
+    systemPage.append(createUserBlock);
+    systemPage.append(usersListBlock);
+
+    document.body.append(systemPage);
+}
+
+async function fetchUsersList() {
+    config.method = 'get';
+    config.url = "/api/users/list";
+
+    var body = null;
+
+    await axios(config)
+        .then(function (response) {
+            body = response.data;
+        })
+        .catch(function (response) {
+            body = null;
+        });
+
+    return body;
+}
+
+function buildUserRow(num, user) {
+    var tr = document.createElement('tr');
+
+    var className = "font-header-6";
+
+    var td0 = document.createElement('td');
+    td0.textContent = parseInt(num) + 1;
+    td0.className = className;
+
+    var td1 = document.createElement('td');
+    td1.textContent = user["username"];
+    td1.className = className;
+
+    var td2 = document.createElement('td');
+    td2.textContent = user["firstName"];
+    td2.className = className;
+
+    var td3 = document.createElement('td');
+    td3.textContent = user["lastName"];
+    td3.className = className;
+
+    var td4 = document.createElement('td');
+    td4.textContent = user["email"];
+    td4.className = className;
+
+    var td5 = document.createElement('td');
+    td5.textContent = user["role"];
+    td5.className = className;
+
+    tr.append(td0);
+    tr.append(td1);
+    tr.append(td2);
+    tr.append(td3);
+    tr.append(td4);
+    tr.append(td5);
+
+    return tr;
+}
+
+function createUser() {
+    config.method = "post";
+    config.url = "/api/users/create";
+    
+    var username = document.getElementById("username").value;
+    var firstName = document.getElementById("first-name").value;
+    var lastName = document.getElementById("last-name").value;
+    var email = document.getElementById("email").value;
+
+    var body = {
+        username: username,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        role: role
+    };
+    config.data = JSON.stringify(body);
+
+    axios(config)
+        .then(function (response) {
+            
+        })
+        .catch(function (response) {
+            
+        });
+}
+
 function buildMainContent(id) {
     var div = document.createElement('div');
     div.id = id;
@@ -248,4 +365,25 @@ function buildInputField(type, id, placeholder) {
     input.id = id;
     input.placeholder = placeholder;
     return input;
+}
+
+function buildButton(title, action) {
+    var button = document.createElement('button');
+    button.className = "button-text z-axis-1 font-button";
+    button.textContent = title;
+    button.onclick = action;
+    return button;
+}
+
+// Array of following elements: {value: "some_value", text: "some_text"}
+function buildDropdown(options) {
+    var select = document.createElement('select');
+    select.className = "dropdown-list";
+    for (option in options) {
+        var opt = document.createElement('option');
+        opt.value = options[option]["value"];
+        opt.text = options[option]["text"];
+        select.append(opt);
+    }
+    return select;
 }
