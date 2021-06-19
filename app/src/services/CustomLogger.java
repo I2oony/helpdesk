8a1587e0845fed000e08765ba07e1bdc39cae4e0
I2oony <ppalamar28@gmail.com>
2021-06-19 03:24:25 +0300

    back: added logging to file

--- app/src/services/CustomLogger.java
+++ app/src/services/CustomLogger.java
@@ -1,5 +1,6 @@
 package services;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -7,30 +8,48 @@ import java.util.logging.*;
 
 public class CustomLogger extends Logger {
 
+    private static Formatter formatter = new Formatter() {
+    @Override
+    public String format(LogRecord record) {
+        Date now = new Date(record.getMillis());
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String log = "";
+        log = log
+                .concat("[")
+                .concat(dateFormat.format(now))
+                .concat("] - ")
+                .concat(record.getLoggerName())
+                .concat(" - ")
+                .concat(record.getLevel().toString())
+                .concat(" - ")
+                .concat(record.getMessage())
+                .concat("\n");
+        return log;
+    }
+    };
+
+    private static FileHandler fileHandler = createFileHandler(formatter);
+
     public CustomLogger(String loggerName) {
         super(loggerName, null);
         this.setLevel(Level.INFO);
+
         ConsoleHandler handler = new ConsoleHandler();
         handler.setLevel(Level.INFO);
-        handler.setFormatter(new Formatter() {
-            @Override
-            public String format(LogRecord record) {
-                Date now = new Date(record.getMillis());
-                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-                String log = "";
-                log = log
-                        .concat("[")
-                        .concat(dateFormat.format(now))
-                        .concat("] - ")
-                        .concat(record.getLoggerName())
-                        .concat(" - ")
-                        .concat(record.getLevel().toString())
-                        .concat(" - ")
-                        .concat(record.getMessage())
-                        .concat("\n");
-                return log;
-            }
-        });
+        handler.setFormatter(formatter);
         this.addHandler(handler);
+        this.addHandler(fileHandler);
+    }
+
+    public static FileHandler createFileHandler(Formatter formatter) {
+        try {
+            FileHandler fileHandler = new FileHandler("logs_%u.%g.log", 100000, 20, true);
+            fileHandler.setLevel(Level.INFO);
+            fileHandler.setFormatter(formatter);
+            return fileHandler;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
